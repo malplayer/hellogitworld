c0c36f344b02488457dc678428fc165ce58105e5
Matthew McCullough <matthewm@ambientideas.com>
2011-05-14 14:37:32 -0500

    Bug fix

--- src/Main.groovy
+++ src/Main.groovy
@@ -9,6 +9,6 @@ int programmingPoints = 10
 println "Hello ${name}"
 println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${square(programmingPoints)}"
-println "${programmingPoints} divided by 2 bonus points is ${divide(programmingPoints, 2)}"
+println "${programmingPoints} divided by 10 bonus points is ${divide(programmingPoints, 10)}"
 println "${programmingPoints} minus 7 bonus points is ${subtract(programmingPoints, 7)}"
 println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"
